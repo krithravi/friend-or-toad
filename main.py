38e5c19b8e35a6c66af28d8e45fa0683d00bdaba
krithravi <krithika.x.ravishankar@gmail.com>
2021-04-04 13:57:25 -0500

    accepts one command line param

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import numpy as np
-import os, cv2
+import os, cv2, sys, getopt
 # some handfulls of happy arrays!
 
 data = []
@@ -96,3 +96,38 @@ model.fit(x_train,y_train,batch_size=50,epochs=100,verbose=1)
 # test the thing
 score = model.evaluate(x_test, y_test, verbose=1)
 print('\n', 'Test accuracy:', score[1])
+
+
+# predict on a single img
+def convert_to_array(img):
+    im = cv2.imread(img)
+    img = Image.fromarray(im, 'RGB')
+    image = img.resize((50, 50))
+    return np.array(image)
+
+def get_animal_name(label):
+    if label==0:
+        return "cane toad"
+    if label==1:
+        return "regular frog"
+
+def predict_animal(file):
+    print("Predicting .................................")
+    ar=convert_to_array(file)
+    ar=ar/255
+    label=1
+
+    a=[]
+    a.append(ar)
+    a=np.array(a)
+    score=model.predict(a,verbose=1)
+    print(score)
+
+    label_index=np.argmax(score)
+    print(label_index)
+    acc=np.max(score)
+    animal=get_animal_name(label_index)
+    print("This frog is a " + animal + " with accuracy = "+str(acc))
+
+
+predict_animal(sys.argv[1])
